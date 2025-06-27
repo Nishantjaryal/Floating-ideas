89477d7a479333ce05f6c66a823994a33ab946cb
Nishantjaryal <nishantjaryal24@gmail.com>
2025-06-27 18:31:27 +0530

    Initial commit

---
+++ app/components/background.jsx
@@ -0,0 +1,17 @@
+'use client'
+
+const background = () => {
+  return (
+    <div
+    className="w-full h-full bg-zinc-800 fixed z-[2]"
+    >
+     
+      <h1
+        className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] text-zinc-900 font-semibold tracking-tighter leading-none ">
+        Docs.
+      </h1>
+    </div>
+  )
+}
+
+export default background
